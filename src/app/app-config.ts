f47020dc19382268bc39189cce5a30e7a8c62e6a
p4cm4n972 <manuel.adele@gmail.com>
2018-03-21 15:29:12 +0100

    app config

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -2,7 +2,7 @@
 export const Url = {
     server: 'http://localhost:5000',
     borneForPayment: 'http://10.1.1.144:9010/ws/payment',
-    borneForDataticket: 'http://10.1.1.144:9010/ws/payment',
+    borneForDataticket: 'http://10.1.1.144:9010/ws/dataticket',
     borneForDispenser: 'http://10.1.1.144:9010/ws/dispenser',
     borneForHeartbeat: 'http://10.1.1.144:9010/ws/heartbeat',
 };
