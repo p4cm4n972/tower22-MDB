235fc6aa3808903837141be2327f311f95e60ffe
p4cm4n972 <manuel.adele@gmail.com>
2018-03-28 10:42:05 +0200

    socket close

--- server.js
+++ server.js
@@ -174,6 +174,6 @@ io.on("connection", function (socket) {
     console.log(`SERVER ${socket.id} +  'user disconnected`);
   });
   app.post('/ws/status', function (req, res) {
-    console.log(req.body);
+    console.log(req.body.Mode);
   })
 });
